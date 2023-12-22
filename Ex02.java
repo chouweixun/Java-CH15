c544aec371b0e7c6e318eec6272170a7c3ec8c33
chouweixun <145731433+chouweixun@users.noreply.github.com>
2023-12-22 11:09:42 +0800

    Create Ex02.java

--- Ex02.java
+++ Ex02.java
@@ -5,7 +5,7 @@ class Test extends Thread{
     }
     public void run(){
         for(int i =1;i<=5;i++){
-            for(int j=0;j<100000000;j++);
+            for(int j=0;j<10000000;j++);
         System.out.println(id+" "+i);
     }
 }
